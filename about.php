0867a917acfe495baa971c61105a6191adde99f3
Killiam <killiam1119@gmail.com>
2025-07-16 07:39:02 -0500

    Estructura web

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre mi</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/post.php">Post</a></li>
+        <li><a href="/about.php">Sobre mi</a></li>
+        <li><a href="/links.php">Proyectos</a></li>
+    </ul>
+    
+    <h1>Sobre mi</h1>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/post.php">Post</a></li>
+        <li><a href="/about.php">Sobre mi</a></li>
+        <li><a href="/links.php">Proyectos</a></li>
+    </ul>
+    
+    <h1>Post</h1>
+</body>
+</html>
