81fc00e989bfaf55bf15a8b4b7a6507807731dee
Killiam <killiam1119@gmail.com>
2025-07-16 08:28:20 -0500

    Activar botones de menú dinámicamente con PHP

--- resources/partials/navbar.php
+++ resources/partials/navbar.php
@@ -1,10 +1,15 @@
 <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl flex h-16 items-center justify-center">
         <div class="flex gap-4">
-            <a href="/"          class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Inicio</a>
-            <a href="/post.php"  class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Post</a>
-            <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Acerca de</a>
-            <a href="/links.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Proyectos</a>
+            <a href="/"class="<?= $_SERVER['REQUEST_URI'] === '/'                    ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Inicio</a>
+            <a href="/post.php"  class="<?= $_SERVER['REQUEST_URI'] === '/post.php'  ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?>rounded-md px-3 py-2 text-sm font-medium">Post</a>
+            <a href="/about.php" class="<?= $_SERVER['REQUEST_URI'] === '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Acerca de</a>
+            <a href="/links.php" class="<?= $_SERVER['REQUEST_URI'] === '/links'     ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Proyectos</a>  
         </div>
     </div>
 </nav>
+
+<?php
+/* var_dump($_SERVER['REQUEST_URI']);
+die(); */
+?>
